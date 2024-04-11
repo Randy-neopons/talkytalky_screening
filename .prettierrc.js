72dce5f55efd408bbccd9cafb772b89ca4d9894f
Randy-neopons <randy@neopons.com>
2024-04-11 17:37:33 +0900

    말운동평가 프로젝트 초기세팅
    feat
    - next.js page router
    - tailwind css
    - react-query
    - eslint, prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    printWidth: 140, // 가로 줄 길이
+    tabWidth: 4, // 탭 간격
+    useTabs: false, // 탭 대신 공백 사용
+    singleQuote: true, // 작은따옴표
+    jsxSingleQuote: true, // jsx 내에서도 작은따옴표 사용
+    trailingComma: 'all', // 마지막 줄에도 콤마 사용
+    bracketSpacing: true, // 중괄호 사이에 공백
+    semi: true, // 세미콜론 붙임
+    arrowParens: 'avoid', // arrow function 가능한 경우엔 괄호 생략
+    endOfLine: 'auto', // EoF 설정
+    formatOnSave: true,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <HydrationBoundary state={pageProps.dehydratedState}>
+                <Component {...pageProps} />
+            </HydrationBoundary>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
