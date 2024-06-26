fe6f593d4e63575da556743e8161066a79501824
Randy-neopons <118699828+Randy-neopons@users.noreply.github.com>
2024-06-26 19:09:49 +0900

    디자인 시스템, 공통 컴포넌트
    feat
    - theme 추가
    - 폰트 추가
    - 버튼 css 추가
    - checkbox css 추가 진행중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,9 @@ export default function Home() {
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
             <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-                <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+                <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-noto'>
                     Get started by editing&nbsp;
-                    <code className='font-mono font-bold'>src/pages/index.tsx</code>
+                    <code className='font-mono font-bold text-head-1'>src/pages/index.tsx</code>
                 </p>
                 <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
                     <a
@@ -19,6 +19,21 @@ export default function Home() {
                         rel='noopener noreferrer'
                     >
                         By <Image src='/vercel.svg' alt='Vercel Logo' className='dark:invert' width={100} height={24} priority />
+                        <button className='btn btn-large btn-outlined'>ABC</button>
+                        <label className='flex justify-center cursor-pointer checkbox-label'>
+                            <input type='checkbox' className='hidden peer' readOnly />
+                            <svg
+                                className='hidden text-white peer-checked:block'
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='24'
+                                height='24'
+                                viewBox='0 0 24 24'
+                                fill='none'
+                            >
+                                <path d='M7 12L11 16L17 8' stroke='white' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+                            </svg>
+                            안녕
+                        </label>
                     </a>
                 </div>
             </div>
